c5a08f214ed54b07aeece2797fdba21f1ef2fd99
Sachin Mane <czx2816@varian.com>
2023-01-27 19:03:40 +0530

    hello

---
+++ src/main/java/com/example/jaan/JaanApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jaan/JaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
